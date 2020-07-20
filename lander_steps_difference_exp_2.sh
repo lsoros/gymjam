fa565bfa1bc4156dd9669c6bec1ac0540eb585fc
Logan Walker <67067024+walkerl27@users.noreply.github.com>
2020-07-20 11:44:12 -0400

    Experiments with Lander Steps difference : Sizer range (2,200)

---
+++ lander_steps_difference_exp_2.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+#SBATCH --verbose
+#SBATCH --job-name=lunE14
+#SBATCH --output=slurm_%j.out
+#SBATCH --error=slurm_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=1GB
+
+# /bin/hostname
+# /bin/pwd
+
+# #module load python3/intel/3.6.3
+
+# eval "$(pyenv init -)"
+# pyenv activate lunarlander
+
+# Where results are going to be written
+# OUTDIR=/scratch/od356/lunarlander_experiments_03
+# Make changes to this OUTDIR as required
+OUTDIR=/home/logan/gym/gymjam/lander_steps_output
+
+# /home/logan/gym/gymjam/lander_steps_output
+CHECKPOINT_FREQ=100
+
+#NUM_INDIVIDUALS=1000 #test run
+NUM_INDIVIDUALS=100000 #real experiment
+SIZER_RANGE=200
+
+for n in {1..20}
+do
+    python3.7 lunarlandercolab.py \
+           --run-id=e1_$n \
+           --search-type=ME \
+           --mode='ME-endpointBC' \
+           --init-population-size=1000 \
+           --num-individuals=$NUM_INDIVIDUALS \
+           --checkpoint-dir=$OUTDIR \
+           --checkpoint-prefix=landerStepsDiff_$n \
+           --checkpoint-enabled \
+           --checkpoint-frequency=$CHECKPOINT_FREQ \
+           --seed=1008
+done
