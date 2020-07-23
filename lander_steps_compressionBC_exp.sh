4d473f8ac21957f200d0eecd43400ac5893a3d02
Prasanna <prasanna.sachin@yahoo.co.in>
2020-07-23 14:18:42 -0400

    Adding shell script to run experiments with the compression BC

---
+++ lander_steps_compressionBC_exp.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+#SBATCH --verbose
+#SBATCH --job-name=lunE14
+#SBATCH --output=slurm_%j.out
+#SBATCH --error=slurm_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=1GB
+
+# /bin/hostname
+# /bin/pwd
+
+# #module load python3/intel/3.6.3
+
+# eval "$(pyenv init -)"
+# pyenv activate lunarlander
+
+# Where results are going to be written
+# OUTDIR=/scratch/od356/lunarlander_experiments_03
+# Make changes to this OUTDIR as required
+OUTDIR=/Users/bharathsurianarayanan/Desktop/gymjam/lander_steps_zlibcompressionBC_stats
+
+# /Users/bharathsurianarayanan/Desktop/gymjam/lander_steps_output
+# /Users/bharathsurianarayanan/Dropbox/My Mac (Bharath’s MacBook Pro)/Desktop/gymjam
+CHECKPOINT_FREQ=100
+
+#NUM_INDIVIDUALS=1000 #test run
+NUM_INDIVIDUALS=100000 #real experiment
+SIZER_RANGE=200
+
+for n in {1..20}
+do
+    python lunar_lander_compressionBC.py \
+           --run-id=comppressionBC1_$n \
+           --search-type=ME \
+           --mode='ME-endpointBC' \
+           --init-population-size=1000 \
+           --num-individuals=$NUM_INDIVIDUALS \
+           --checkpoint-dir=$OUTDIR \
+           --checkpoint-prefix=compressionBC_$n \
+           --checkpoint-enabled \
+           --checkpoint-frequency=$CHECKPOINT_FREQ \
+           --seed=1008
+done
